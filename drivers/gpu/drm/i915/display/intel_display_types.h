2488f3f87cf86d9cc7d886718040d9fe7a5b9d5f
Lyude Paul <lyude@redhat.com>
2021-01-14 17:17:05 -0500

    drm/i915: Pass port to intel_panel_bl_funcs.get()
    In the next commit where we split PWM related backlight functions from
    higher-level backlight functions, we'll want to be able to retrieve the
    backlight level for the current display panel from the
    intel_panel_bl_funcs->setup() function using pwm_funcs->get(). Since
    intel_panel_bl_funcs->setup() is called before we've fully read in the
    current hardware state into our atomic state, we can't grab atomic
    modesetting locks safely anyway in intel_panel_bl_funcs->setup(), and some
    PWM backlight functions (vlv_get_backlight() in particular) require knowing
    the currently used pipe we need to be able to discern the current display
    pipe through other means. Luckily, we're already passing the current
    display pipe to intel_panel_bl_funcs->setup() so all we have to do in order
    to achieve this is pass down that parameter to intel_panel_bl_funcs->get().
    
    So, fix this by accepting an additional pipe parameter in
    intel_panel_bl_funcs->get(), and leave figuring out the current display
    pipe up to the caller.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210114221709.2261452-2-lyude@redhat.com

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -220,6 +220,17 @@ struct intel_encoder {
 	const struct drm_connector *audio_connector;
 };
 
+struct intel_panel_bl_funcs {
+	/* Connector and platform specific backlight functions */
+	int (*setup)(struct intel_connector *connector, enum pipe pipe);
+	u32 (*get)(struct intel_connector *connector, enum pipe pipe);
+	void (*set)(const struct drm_connector_state *conn_state, u32 level);
+	void (*disable)(const struct drm_connector_state *conn_state, u32 level);
+	void (*enable)(const struct intel_crtc_state *crtc_state,
+		       const struct drm_connector_state *conn_state, u32 level);
+	u32 (*hz_to_pwm)(struct intel_connector *connector, u32 hz);
+};
+
 struct intel_panel {
 	struct drm_display_mode *fixed_mode;
 	struct drm_display_mode *downclock_mode;
